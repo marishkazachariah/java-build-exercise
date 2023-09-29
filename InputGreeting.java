7af6e548682756ae8a031d0e6f8eadbfd3ef70e3
marishkazachariah <marishka.zachariah@gmail.com>
2023-09-29 11:37:23 +0200

    complete method for InputGreeting class

--- InputGreeting.java
+++ InputGreeting.java
@@ -1,5 +1,14 @@
 public class InputGreeting{
 	public static void main(String[] args){
-		
+		String input = "";
+		System.out.println("What is your name?");
+		input = System.console().readLine();
+		System.out.println("Hello " + input);
+
+		while(!input.equals("exit")) {
+			System.out.println("What is your name?");
+			input = System.console().readLine();
+			System.out.println("Hello " + input);
+		}
 	}
 }
